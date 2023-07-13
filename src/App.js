f5b7ba005f749b5330ae90acac9725cf50a2506e
rudgns1104 <116240115+rudgns1104@users.noreply.github.com>
2023-07-13 20:24:53 +0900

    App.js 수정이랑 뭐 npm install함

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import {Route, Routes, Link} from "react-router-dom"
+import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
-import Header from './Header';
-import Footer from './Footer';
-import Main from './component/Main';
-import Detail from './component/Detail';
-import Player from './component/Player';
-import Weapon from './component/Weapon';
-
+import Header from "./Header";
+import Footer from "./Footer";
+import Main from "./component/Main";
+import Detail from "./component/Detail";
+import Player from "./component/Player";
+import Weapon from "./component/Weapon";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      
+
       <AnimatePresence>
         <Routes>
           <Route path="/" element={<Main />} />
@@ -23,7 +22,6 @@ function App() {
         </Routes>
       </AnimatePresence>
       <Footer />
-      
     </div>
   );
 }
